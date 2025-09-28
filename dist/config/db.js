708396f1fce4385c14d56925347983518562136c
Gasto <gaston15.isma@gmail.com>
2025-09-28 20:46:54 -0300

    correccion back

--- dist/config/db.js
+++ dist/config/db.js
@@ -5,13 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectDB = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
-const character_service_1 = require("../services/character.service");
 const connectDB = async () => {
     try {
         await mongoose_1.default.connect(process.env.MONGO_URI);
         console.log("✅ MongoDB conectado");
-        // Inicializar personajes en la base de datos
-        await character_service_1.CharacterService.seedCharacters();
     }
     catch (err) {
         console.error("❌ Error conectando MongoDB", err);
